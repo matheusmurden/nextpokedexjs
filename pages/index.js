c7ef84ff9b287bc73754457b4997a16b63583023
Matheus Murden <matheus@murden.dev>
2021-02-15 14:09:53 -0300

    initial integration of pokémon sounds

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,19 @@ export default function NextPokedex({ list }) {
       </Head>
 
       {
-        list.map(({ name }, index) => (
-          <figure key={index}>
+        list.map(({ id, name, icon, artwork, sounds }, index) => (
+          <figure key={id}>
             <figcaption>{name}</figcaption>
             <Image
-              width="280"
-              height="280"
+              width="40"
+              height="40"
               loading="lazy"
-              src={`/assets/artwork/${index + 1}.png`}
+              src={icon}
             />
+            <audio controls preload="metadata">
+              <source src={sounds.ogg} type="audio/ogg; codecs=vorbis"/>
+              <source src={sounds.mp3} type="audio/mpeg"/>
+            </audio>
           </figure>
         ))
       }
@@ -29,7 +33,22 @@ export default function NextPokedex({ list }) {
 export async function getStaticProps() {
   const list = await fetch('https://pokeapi.co/api/v2/pokemon?limit=898')
     .then(res => res.json())
-    .then(({ results }) => results)
+    .then(({ results }) => results.map(
+      ({ name, url }) => {
+        const id = parseInt(url.split('/')[url.split('/').length - 2], 10)
+        const type = id > 10000 ? 'alternate' : 'regular'
+        return {
+          name,
+          id,
+          icon: `/assets/icons/${type}/${id}.png`,
+          artwork: `/assets/artwork/${type}/${id}.png`,
+          sounds: {
+            mp3: `/assets/sounds/mp3/${type}/${id}.mp3`,
+            ogg: `/assets/sounds/ogg/${type}/${id}.ogg`
+          }
+        }
+      }
+    ))
     .catch(e => { throw new Error(e.message || e) })
 
   return {
